d7340143dccfd02a1707135bfebfca8813ca6487
Chavanus <chavanus.t@generaloutsourcing.co.th>
2024-05-16 17:21:34 +0700

    play time

--- index.js
+++ index.js
@@ -12,15 +12,37 @@ class ABBB {
   a = new AAAA();
 }
 function main() {
-  let config = new ABBB();
-  config = null;
-  p();
-}
+  let arrStr = [];
+  const loop = 10 ^ 5;
+  for (let i = 0; i < loop; i++) {
+    const prop = {
+      a: i,
+    };
+    arrStr.push(prop);
+  }
 
-function p(config) {
-  const abc = config?.a?.b ?? "Hello";
-  console.log(abc);
+  console.time("lo1");
+  const size = arrStr.length;
+  let sum = 0;
+  for (let i = 0; i < size; i++) {
+    sum += arrStr[i].a;
+  }
+  console.timeEnd("lo1");
+
+  console.time("lo2");
+  let sum2 = 0;
+  arrStr.map((x) => {
+    sum2 += x.a;
+  });
+  console.timeEnd("lo2");
+
+  console.time("lo2");
+  let sum3 = 0;
+  for (const prop of arrStr) {
+    sum3 += prop.a;
+  }
+  console.timeEnd("lo2");
 }
 
 main();
-process.exit(1);
+process.exit(0);
