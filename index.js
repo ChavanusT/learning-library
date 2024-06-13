2af54e2514f47ecaae4ccb2abf758d95e0420043
Chavanus <chavanus.t@generaloutsourcing.co.th>
2024-06-13 10:32:58 +0700

    add Datetimeoffset

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const customParseFormat = require("dayjs/plugin/customParseFormat");
 const utc = require("dayjs/plugin/utc");
 const short = require("short-uuid");
 const ping = require("ping");
+const time = require("./playground3");
+const tz = require("dayjs/plugin/timezone");
 
 dayjs.extend(customParseFormat);
 dayjs.extend(utc);
+dayjs.extend(tz);
 
 const express = require("express");
 const { default: axios } = require("axios");
@@ -14,15 +17,70 @@ const port = 3000;
 
 const L = require("./playground");
 const cds = require("./playground2");
+const readline = require("node:readline");
 
-function main() {
+async function main() {
     //L();
-    cds();
+    //cds();
+    await time.start();
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const promise = new Promise((resolve, reject) => {
+        try {
+            rl.question("Want to insert ?", async (e) => {
+                if (e == "yes" || e == "y") {
+                    console.log("Insert Time!!!");
+                    const dateTime2 = dayjs().format();
+                    // const timestamp = dayjs().unix();
+                    const timeTest = dayjs().format("HH:mm:ss.SSS");
+                    const date = dayjs().tz(dayjs.tz.guess());
+                    const offset = date.format("Z");
+                    const dateTimeOffset = date.format(
+                        `YYYY-MM-DD HH:mm:ss.SSS ${offset}`
+                    );
+
+                    console.log(dateTime2, timeTest, dateTimeOffset);
+                    await time.insertTime(dateTime2, timeTest, dateTimeOffset);
+                } else {
+                    console.log("No insert Time...");
+                }
+
+                rl.question("Want to Query ?", async (e) => {
+                    console.log(
+                        "I suggest it Query Time!!! No matter what are you do."
+                    );
+                    const result = await time.queryTime();
+                    logResult(result);
+                    rl.close();
+                    resolve("CloseBt122$43");
+                });
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+
+    promise.then((x) => {
+        console.log(`Kono process wa owari da. : ${x}`);
+    });
 }
 
-function bootstrap() {
+function logResult(time) {
+    console.log(`query result : ${JSON.stringify(time[0])}`);
+    // const buffer = Buffer.from(time[0].test_time_stamp);
+    // const bigInt = buffer.readBigInt64BE();
+    // console.log(bigInt);
+    // const timestamp = dayjs.unix(Number(bigInt) * 1000);
+    // console.log(timestamp.format());
+}
+
+function bootstrap(callback) {
     app.listen(port, () => {
         console.log(`Server Start at Port ${port}`);
+        callback?.();
     });
 }
 
@@ -57,6 +115,6 @@ async function testing() {
         });
 }
 
-main();
-bootstrap();
+bootstrap(main);
+
 // testing();
---
+++ playground3.js
@@ -0,0 +1,79 @@
+const { Time, DateTime2, DateTimeOffset } = require("mssql");
+const orm = require("typeorm");
+
+const dataRepo = {
+    time: null,
+};
+
+const timeEntity = new orm.EntitySchema({
+    name: "Time",
+    tableName: "TimeTest_Not_Use_In_PROD",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true,
+        },
+        test_date: { type: "datetime2" },
+        test_time_stamp: {
+            type: "timestamp",
+        },
+        time: { type: "time" },
+        time_offset: { type: "datetimeoffset" },
+    },
+});
+
+async function sqlConnect() {
+    const AppDataSource = new orm.DataSource({
+        type: "mssql",
+        host: "192.168.20.40",
+        port: 1433,
+        username: "admindb99",
+        password: "ktH&g2j54*df%0P",
+        database: "GO_HRIS",
+        logging: false,
+        entities: [timeEntity],
+        ssl: false,
+        synchronize: false,
+        options: {
+            encrypt: false,
+        },
+    });
+
+    await AppDataSource.initialize().then((r) => {
+        console.log("Connect at db");
+    });
+
+    return AppDataSource;
+}
+
+async function queryTime(db) {
+    const timeData = await dataRepo.time.find();
+    return timeData;
+}
+
+async function insertTime(dateTime2, time, offset) {
+    const insert = {
+        test_date: dateTime2,
+        time: time,
+        time_offset: offset,
+    };
+
+    await dataRepo.time.save(insert);
+}
+
+async function start() {
+    try {
+        const db = await sqlConnect();
+        initDataRepo(db);
+        return db;
+    } catch (error) {
+        console.log(`Error at :` + error);
+    }
+}
+
+function initDataRepo(db) {
+    dataRepo.time = db.getRepository("Time");
+}
+
+module.exports = { start, queryTime, insertTime };
